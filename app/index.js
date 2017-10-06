19ff56d2961f57a753cf36cc2729098029c4c2a3
Dan Constantinescu <dan.constantinescu@softvision.ro>
2017-10-06 17:02:45 +0300

    installed all the dependencies

--- app/index.js
+++ app/index.js
@@ -1,2 +1,14 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+
+export default class App extends Component {
+    render(){
+        return(
+            <div>
+                hello
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App/>, document.getElementById('app'));
