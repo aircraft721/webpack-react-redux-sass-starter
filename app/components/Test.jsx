6c9a80eca9774c482a3e74971c2567efbed09cf4
Dan Constantinescu <dan_constantinescu@outlook.com>
2017-10-20 11:29:00 +0300

    implemented a clock

--- app/components/Test.jsx
+++ app/components/Test.jsx
@@ -1,10 +1,35 @@
 import React, {Component} from 'react';
 
 export default class Test extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            time: new Date().toLocaleString('en-US', {hour12: false})
+        }
+
+        this.tick = this.tick.bind(this);
+    }
+
+    tick(){
+        this.setState({
+            time: new Date().toLocaleString('en-US', {hour12: false})
+        })
+    }
+
+    componentDidMount(){
+        this.id = setInterval(() => this.tick(), 1000);
+    }
+
+    componentWillUnmount(){
+        clearInterval(this.id);
+    }
+
     render(){
         return (
             <div className='test'>
-                <h3>Test</h3>
+                <h3>Clock</h3>
+                {this.state.time}
             </div>
         );
     }
